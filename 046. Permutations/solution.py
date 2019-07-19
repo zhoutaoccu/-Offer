32ffed882dc191dd6bd77a23c76f644b6915814c
Zhoutaoccu <zhoutaoccu@qq.com>
2019-07-20 02:12:28 +0800

    增加46号题python解法

---
+++ 046. Permutations/solution.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode id=46 lang=python3
+#
+# [46] Permutations,全排列
+# input:List[]
+# output:List[List[]]
+
+class Solution:
+    def permute(self, nums):
+        def dfs(nums,path,res,visited): #path是一个项，res是结果,visited为集合
+            if (len(nums) == len(path)):
+                res = res.append(path+[]) 
+                return 
+            for i in range(len(nums)):
+                if i not in visited:
+                    path.append(nums[i])
+                    visited.add(i)
+                    dfs(nums,path,res,visited)
+                    path.pop()
+                    visited.discard(i)
+        res = []
+        path = []
+        visited =  set([])
+        dfs(nums,path,res,visited)
+        return res
+            
+            
+if __name__ == "__main__":
+    x = [1,2,3]
+    print(Solution().permute(x))
+    assert (Solution().permute(x) == [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]])
+
