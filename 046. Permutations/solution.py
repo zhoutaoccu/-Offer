b97471747ee2235d5bc648d8c079f0b75d434493
Zhoutaoccu <zhoutaoccu@qq.com>
2019-09-10 01:46:25 +0800

    46全排列

--- 046. Permutations/solution.py
+++ 046. Permutations/solution.py
@@ -7,15 +7,16 @@
 
 class Solution:
     def permute(self, nums):
-        def dfs(nums,path,res,visited): #path是一个项，res是结果,visited为集合
+        def dfs(nums,path,res,visited): #path是一个项，res是结果,visited为下标集合
             if (len(nums) == len(path)):
                 res = res.append(path+[]) 
                 return 
             for i in range(len(nums)):
-                if i not in visited:
+                if i not in visited: #nums[i]
                     path.append(nums[i])
                     visited.add(i)
                     dfs(nums,path,res,visited)
+                    # 
                     path.pop()
                     visited.discard(i)
         res = []
@@ -28,5 +29,5 @@ class Solution:
 if __name__ == "__main__":
     x = [1,2,3]
     print(Solution().permute(x))
+    print(len(Solution().permute(x)))
     assert (Solution().permute(x) == [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]])
-
